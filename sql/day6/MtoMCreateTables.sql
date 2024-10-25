441c421ff886f7592488f535481da633f4bf698e
Umar Solanki <uysolanki@gmail.com>
2024-10-25 17:34:07 +0530

    day 7

--- sql/day6/MtoMCreateTables.sql
+++ sql/day6/MtoMCreateTables.sql
@@ -12,7 +12,6 @@ ryear int(3),
 revenue int(4) check(revenue>0),
 primary key (mno));
 
-
 create table imdb(
 ano int(5),
 mno int(5),
@@ -20,3 +19,25 @@ fees int(5) check(fees>0),
 primary key(mno,ano),
 foreign key(ano) references actor(ano),
 foreign key(mno) references movie(mno));
+
+
+insert into actor(aname,gender,age) values
+('SRK','m',58),
+('AK','m',55),
+('DP','f',40),
+('PC','f',38);
+
+
+insert into movie(mname,ryear,revenue) values
+('CE',2010,500),
+('CCTC',2005,150),
+('DON',2012,300);
+
+
+insert into imdb(ano,mno,fees) values
+(1,1,100),
+(1,3,80),
+(3,1,60),
+(2,2,20),
+(3,2,10),
+(4,3,60);
